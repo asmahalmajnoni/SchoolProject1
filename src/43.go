bf4979273955895196d6b839a6181b248512ab37
asmahalmajnoni <asmahalmajnoni@gmail.com>
2025-05-25 18:31:39 +0000

    Add 43.go

---
+++ src/43.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "Student"
+    var age int = 18
+    var isMarked bool = true
+
+    fmt.Println("Hello", name, ", you are", age, "years old and have been marked as", isMarked)
+}
